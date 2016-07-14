0177ccd693adba50a61fe4b393a7594e90bca16e
Jesse Claven <jesse.claven@gmail.com>
2016-07-14 16:24:13 +0100

    Add some checks to ensure that we aren't creating more than 1 power assertion.

--- Sources/PreventSleep.swift
+++ Sources/PreventSleep.swift
@@ -28,7 +28,9 @@ public class PreventSleep {
     }
     
     deinit {
-        self.allowSleep()
+        if !self.canSleep() {
+            self.allowSleep()
+        }
     }
     
     // MARK: Power assertion handling
@@ -36,6 +38,10 @@ public class PreventSleep {
     // Prevent the computer going to sleep
     // Returns whether the power assertion was successful or not.
     @objc public func preventSleep() -> Bool {
+        if !self.canSleep() {
+            return true
+        }
+
         self.sleepAssertion = IOPMAssertionCreateWithName(
             self.sleepAssertionType,
             IOPMAssertionLevel(kIOPMAssertionLevelOn),
@@ -53,6 +59,10 @@ public class PreventSleep {
     // Allow the computer to go to sleep
     // Returns whether the power assertion was successful or not.
     @objc public func allowSleep() -> Bool {
+        if self.canSleep() {
+            return true
+        }
+
         let sleepAssertionRelease = IOPMAssertionRelease(self.sleepAssertionID!)
         
         if sleepAssertionRelease == kIOReturnSuccess {
@@ -65,7 +75,7 @@ public class PreventSleep {
     // Can the computer go to sleep, or is it being prevented?
     @objc public func canSleep() -> Bool {
         // Check if the assertion already exists
-        if sleepAssertion == kIOReturnSuccess {
+        if self.sleepAssertion == kIOReturnSuccess {
             return false
         } else {
             return true
